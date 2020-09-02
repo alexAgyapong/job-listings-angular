1788d3b05b72cafdd59d412698613638743db422
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2020-09-02 11:47:40 +0100

    Add remove underscore pipe

---
+++ src/app/shared/pipes/remove-underscore.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'removeUnderscore'
+})
+export class RemoveUnderscorePipe implements PipeTransform {
+
+  transform(value: string, ...args: unknown[]): string {
+    if (value) {
+      return value.replace(/_/g, ' ');
+    }
+  }
+
+}
